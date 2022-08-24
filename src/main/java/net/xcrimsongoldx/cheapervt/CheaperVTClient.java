6db7dc2294fbf6bb01465830773bf6fbe67f5124
Peter Wheaton <xcrimsongoldx@gmail.com>
2022-08-24 08:04:21 -0400

    Initial commit

---
+++ src/main/java/net/xcrimsongoldx/cheapervt/CheaperVTClient.java
@@ -0,0 +1,10 @@
+package net.xcrimsongoldx.cheapervt;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CheaperVTClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/xcrimsongoldx/cheapervt/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.xcrimsongoldx.cheapervt.mixin;
+
+import net.xcrimsongoldx.cheapervt.CheaperVillagerTrades;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		CheaperVillagerTrades.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
